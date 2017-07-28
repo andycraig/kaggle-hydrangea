a101ec21bfdedbae768670d17d8ee02891f20a63
Andrew <apc53c@gmail.com>
2017-07-28 22:13:18 +0100

    Fixed broken path in config files. preprocess_gbt.py now runs.

--- preprocess_gbt.py
+++ preprocess_gbt.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
 from PIL import Image
+from PIL import ImageStat
+import scipy
+import scipy.stats
 import yaml
 import sys
 import pandas as pd
 import numpy as np
 
 def main(config_file):
-    with open(config_file, 'r') as f:
-        config = yaml.load(f)
+	with open(config_file, 'r') as f:
+		config = yaml.load(f)
 
 	# Load file of training image names and correct labels.
 	train_set = pd.read_csv(config['train_set'])
@@ -18,61 +21,58 @@ def main(config_file):
 
 	print("Preprocessing GBT training set features...")
 	train_features_gbt = preprocess_gbt(config['train_imgs'], train_set)
-	train_features_gbt.to_csv(config['train_features_gbt'])
+	pd.DataFrame.from_dict(train_features_gbt, orient="index").to_csv(config['train_features_gbt'], index=False, header=False)
 	print("Done.")
 	print("Preprocessing GBT test set features...")
 	test_features_gbt = preprocess_gbt(config['test_imgs'], test_set)
-	test_features_gbt.to_csv(config['test_features_gbt'])
+	pd.DataFrame.from_dict(test_features_gbt, orient="index").to_csv(config['test_features_gbt'], index=False, header=False)
 	print("Done.")
 
 def get_features(path):
-	try:
-		ft = []
-		img = Image.open(path)
-		im_stats_ = ImageStat.Stat(img)
-		ft += im_stats_.sum
-		ft += im_stats_.mean
-		ft += im_stats_.rms
-		ft += im_stats_.var
-		ft += im_stats_.stddev
-		img = np.array(img)[:,:,:3]
-		ft += [scipy.stats.kurtosis(img[:,:,0].ravel())]
-		ft += [scipy.stats.kurtosis(img[:,:,1].ravel())]
-		ft += [scipy.stats.kurtosis(img[:,:,2].ravel())]
-		ft += [scipy.stats.skew(img[:,:,0].ravel())]
-		ft += [scipy.stats.skew(img[:,:,1].ravel())]
-		ft += [scipy.stats.skew(img[:,:,2].ravel())]
-		# bw = cv2.equalizeHist(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY))
-		# ft += list(cv2.HuMoments(cv2.moments(bw)).flatten())
+	ft = []
+	img = Image.open(path)
+	im_stats_ = ImageStat.Stat(img)
+	ft += im_stats_.sum
+	ft += im_stats_.mean
+	ft += im_stats_.rms
+	ft += im_stats_.var
+	ft += im_stats_.stddev
+	img = np.array(img)[:,:,:3]
+	ft += [scipy.stats.kurtosis(img[:,:,0].ravel())]
+	ft += [scipy.stats.kurtosis(img[:,:,1].ravel())]
+	ft += [scipy.stats.kurtosis(img[:,:,2].ravel())]
+	ft += [scipy.stats.skew(img[:,:,0].ravel())]
+	ft += [scipy.stats.skew(img[:,:,1].ravel())]
+	ft += [scipy.stats.skew(img[:,:,2].ravel())]
+	# bw = cv2.equalizeHist(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY))
+	# ft += list(cv2.HuMoments(cv2.moments(bw)).flatten())
 
-		# Histograms:
-		# m.histogram() # This might be the bands concatenated.
-		# Get the bands:
-		#im.split() ⇒ sequence
-		#Returns a tuple of individual image bands from an image. For example, splitting an “RGB” image creates three new images each containing a copy of one of the original bands (red, green, blue).
-		# ft += list(cv2.calcHist([bw],[0],None,[64],[0,256]).flatten()) #bw
-		# ft += list(cv2.calcHist([img],[0],None,[64],[0,256]).flatten()) #r
-		# ft += list(cv2.calcHist([img],[1],None,[64],[0,256]).flatten()) #g
-		# ft += list(cv2.calcHist([img],[2],None,[64],[0,256]).flatten()) #b
-		# m, s = cv2.meanStdDev(img) #mean and standard deviation
-		# ft += list(m.ravel())
-		# ft += list(s.ravel())
+	# Histograms:
+	# m.histogram() # This might be the bands concatenated.
+	# Get the bands:
+	#im.split() ⇒ sequence
+	#Returns a tuple of individual image bands from an image. For example, splitting an “RGB” image creates three new images each containing a copy of one of the original bands (red, green, blue).
+	# ft += list(cv2.calcHist([bw],[0],None,[64],[0,256]).flatten()) #bw
+	# ft += list(cv2.calcHist([img],[0],None,[64],[0,256]).flatten()) #r
+	# ft += list(cv2.calcHist([img],[1],None,[64],[0,256]).flatten()) #g
+	# ft += list(cv2.calcHist([img],[2],None,[64],[0,256]).flatten()) #b
+	# m, s = cv2.meanStdDev(img) #mean and standard deviation
+	# ft += list(m.ravel())
+	# ft += list(s.ravel())
 
-		#Laplacian:
-		#scipy.ndimage.filters.laplace¶
-		# ft += [cv2.Laplacian(bw, cv2.CV_64F).var()]
-		# ft += [cv2.Laplacian(img, cv2.CV_64F).var()]
+	#Laplacian:
+	#scipy.ndimage.filters.laplace¶
+	# ft += [cv2.Laplacian(bw, cv2.CV_64F).var()]
+	# ft += [cv2.Laplacian(img, cv2.CV_64F).var()]
 
-		# Sobel:
-		#image = Image.open('your_image.png')
-		#image = image.filter(ImageFilter.FIND_EDGES)
+	# Sobel:
+	#image = Image.open('your_image.png')
+	#image = image.filter(ImageFilter.FIND_EDGES)
 
-		# ft += [cv2.Sobel(bw,cv2.CV_64F,1,0,ksize=5).var()]
-		# ft += [cv2.Sobel(bw,cv2.CV_64F,0,1,ksize=5).var()]
-		# ft += [cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5).var()]
-		# ft += [cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5).var()]
-	except:
-		print(path)
+	# ft += [cv2.Sobel(bw,cv2.CV_64F,1,0,ksize=5).var()]
+	# ft += [cv2.Sobel(bw,cv2.CV_64F,0,1,ksize=5).var()]
+	# ft += [cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5).var()]
+	# ft += [cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5).var()]
 	return ft
 
 def preprocess_gbt(folder_name, names_and_labels):
