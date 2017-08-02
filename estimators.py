0d55f28bd725ec5e8f3892a21d9faf764ca8eea7
Andrew <apc53c@gmail.com>
2017-08-02 08:28:54 +0100

    Got NN parameters from earlier version. Doesn't work on dummy data, but might work on larger data.

--- estimators.py
+++ estimators.py
@@ -82,11 +82,12 @@ class NN(BaseEstimator, ClassifierMixin):
 		self.model = get_model()
 		earlystop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=15, verbose=0, mode='auto')
 		# Do the fit.
-		batch_size = 64
 		epochs = 1000
+		batch_size = 64
+		steps_per_epoch = len(self.X_) / batch_size
 		self.model.fit_generator(datagen.flow(self.X_4Dmatrix, self.y_, batch_size=batch_size),
 			callbacks=[earlystop],
-			steps_per_epoch=(len(y) / batch_size),
+			steps_per_epoch=steps_per_epoch,
 			epochs=epochs,
 			verbose=2)
 		# Return the classifier
